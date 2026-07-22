47f1148604b05c162078ad158f7722660243d0cc
Riccardo Malchiodi <115894161+Malchio03@users.noreply.github.com>
2026-07-22 20:37:48 +0200

    Player Movement
    Movimento del giocatore in base a dove si trova il cursore

--- Assets/Scripts/Player/TopDownCharacterMove.cs
+++ Assets/Scripts/Player/TopDownCharacterMove.cs
@@ -6,6 +6,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private inputHandler _input;
     [SerializeField] private float moveSpeed;
     [SerializeField] private Camera cam;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private bool rotateTowardsMouse;
 
 
     private void Awake()
@@ -16,18 +18,46 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+        var movementVector = MoveTowardTarget(targetVector);
 
-        // si muove nella direzione che miriamo
-        MoveTowardTarget(targetVector);
-
-       //RotateTowardMovementVect();
+        if (!rotateTowardsMouse)
+            RotateTowardMovementVect(movementVector);
+        else
+            RotateTowardsMouseVector();
     }
 
-    private void MoveTowardTarget(Vector3 targetVector)
+    private void RotateTowardsMouseVector()
+    {
+        Ray ray = cam.ScreenPointToRay(_input.MousePosition);
+
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        {
+            var target = hitInfo.point;
+            target.y = transform.position.y;
+            transform.LookAt(target);
+        }
+    }
+
+    private void RotateTowardMovementVect(Vector3 movementVector)
+    {
+        if (movementVector.magnitude == 0)
+            return;
+        var rotation = Quaternion.LookRotation(movementVector);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed);
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
     {
         var speed = moveSpeed * Time.deltaTime;
-        targetVector = Quaternion.Euler(0, cam.gameObject.transform.eulerAngles.y, 0) * targetVector;
+
+        // se voglio che il personaggio vada verso la direzione del mouse
+        targetVector = transform.TransformDirection(targetVector);
+
+        // muove il personaggio rispetto alla rotazione della telecamera
+        // targetVector = Quaternion.Euler(0, cam.gameObject.transform.eulerAngles.y, 0) * targetVector;
+
         var targetPosition = transform.position + targetVector * speed;
         transform.position = targetPosition;
+        return targetVector;
     }
 }
