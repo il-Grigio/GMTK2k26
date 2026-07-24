2d3535bcd21d8b08b2053d5c4e400b5521f92083
Riccardo Malchiodi <115894161+Malchio03@users.noreply.github.com>
2026-07-24 14:35:38 +0200

    Object Pooling VFX

---
+++ Assets/Scripts/Bullets/EffectPoolManager.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class EffectPoolManager : MonoBehaviour
+{
+    public static EffectPoolManager Instance { get; private set; }
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string key;
+        public GameObject prefab;
+    }
+
+    public List<Pool> pools = new List<Pool>();
+    private Dictionary<string, List<GameObject>> poolDict = new Dictionary<string, List<GameObject>>();
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    public GameObject Get(string key, Vector3 position, Quaternion rotation)
+    {
+        var pool = pools.FirstOrDefault(p => p.key == key);
+        if (pool == null) return null;
+
+        if (!poolDict.ContainsKey(key))
+            poolDict[key] = new List<GameObject>();
+
+        var list = poolDict[key];
+        GameObject obj = list.FirstOrDefault(o => !o.activeSelf);
+
+        if (obj == null)
+        {
+            obj = Instantiate(pool.prefab);
+            list.Add(obj);
+        }
+
+        obj.transform.SetPositionAndRotation(position, rotation);
+        obj.SetActive(true);
+        return obj;
+    }
+}
