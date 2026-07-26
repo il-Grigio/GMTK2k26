c85b4eea5ea0e016ba8f4358c2fe2c1a2a096bc9
Luigi Bettinazzi <89411673+Kiogra@users.noreply.github.com>
2026-07-26 16:25:17 +0200

    ITEM INFO FIX

--- Assets/Scripts/Inv&Shop/ItemInfoComponent.cs
+++ Assets/Scripts/Inv&Shop/ItemInfoComponent.cs
@@ -1,29 +1,10 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
-[ExecuteAlways]
 public class ItemInfoComponent : MonoBehaviour
 {
-    private static readonly int IsSceneLayerID = Shader.PropertyToID("_IsSceneLayer");
-    [SerializeField] private LayerMask sceneLayerMask;
-
     [SerializeField] int countValue;
 
     [SerializeField] int moneyValue;
-
-    private Renderer _renderer;
-    private MaterialPropertyBlock _mpb;
-
-    private void OnEnable()
-    {
-        Apply();
-    }
-
-    private void OnValidate()
-    {
-        Apply();
-    }
-
     public int GetPointValue()
     {
         Debug.Log("moneyvalue: " + moneyValue + " countvalue: " + countValue + " moneyvalue: " + moneyValue);
@@ -34,15 +15,4 @@ public class ItemInfoComponent : MonoBehaviour
         return new ItemInfoData(countValue, moneyValue);
     }
 
-    public void Apply()
-    {
-        if (_renderer == null) _renderer = GetComponent<Renderer>();
-        if (_mpb == null) _mpb = new MaterialPropertyBlock();
-
-        bool isSceneLayer = (sceneLayerMask.value & (1 << gameObject.layer)) != 0;
-
-        _renderer.GetPropertyBlock(_mpb);
-        _mpb.SetFloat(IsSceneLayerID, isSceneLayer ? 1f : 0f);
-        _renderer.SetPropertyBlock(_mpb);
-    }
 }
